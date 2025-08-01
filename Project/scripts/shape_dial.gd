7593322a47c980fdfc0339f88f2d2612b10ff8d0
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2025-08-01 18:19:51 -0400

    Dial

---
+++ Project/scripts/shape_dial.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+enum Shape
+{
+	CIRCLE,
+	CONE,
+	CROSS,
+	DIAMOND,
+	FUNNEL,
+	SQUARE
+}
+
+func get_sprite(shape) -> Sprite2D:
+	match shape:
+		Shape.CIRCLE: return $ShapeCircle
+		Shape.CONE: return $ShapeCone
+		Shape.CROSS: return $ShapeCross
+		Shape.DIAMOND: return $ShapeDiamond
+		Shape.FUNNEL: return $ShapeFunnel
+		Shape.SQUARE: return $ShapeSquare
+	return null
+
+# pass the Shape enum
+func toggle(shape) -> void:
+	var sprite = get_sprite(shape)
+	sprite.visible = !sprite.visible
+
+func get_dial_code() -> String:
+	var result = ""
+	if $ShapeCircle.visible:
+		result += "CI"
+	if $ShapeCone.visible:
+		result += "CO"
+	if $ShapeCross.visible:
+		result += "CR"
+	if $ShapeDiamond.visible:
+		result += "DI"
+	if $ShapeFunnel.visible:
+		result += "FU"
+	if $ShapeSquare.visible:
+		result += "SQ"
+	return result
+
+func _process(delta: float) -> void:
+	$DebugLabel.text = get_dial_code()
+	pass
