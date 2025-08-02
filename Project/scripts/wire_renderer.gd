308e308e3e95fa0f7f920bf99cd2622c171596ee
Hojaverde <jmprocco@gmail.com>
2025-08-02 16:13:15 -0400

    added set_random_color to wire renderer

--- Project/scripts/wire_renderer.gd
+++ Project/scripts/wire_renderer.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var line2d: Line2D = $Line2D
 
+@export var colors:Array[Color]
+
 func set_points(points:Array[Vector2]):
 	line2d.clear_points()
 	for p in points:
@@ -9,3 +11,7 @@ func set_points(points:Array[Vector2]):
 	
 func set_color(color:Color):
 	line2d.default_color = color
+	
+func set_random_color():
+	set_color(colors[ randi_range(0,colors.size() -1)])
+	
