1b6828d9f244ec0e11226a6b32622c9cd508c83d
Hojaverde <jmprocco@gmail.com>
2025-08-01 21:25:35 -0400

    Added parallax_element

---
+++ Project/scripts/parallax_element.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+var start_offset: Vector2
+var camera:Camera2D
+
+@export var depth:float
+
+func _init() -> void:
+	start_offset = global_position
+	
+	
+func _process(delta: float) -> void:
+	camera = get_viewport().get_camera_2d()
+	var offset_vector:Vector2 =  start_offset - camera.transform.get_origin()
+	var dir = offset_vector.normalized()
+	var dist = offset_vector.length()
+	
+	global_position = start_offset - dir*dist*depth	
+	 
+	
