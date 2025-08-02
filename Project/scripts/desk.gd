764039de9ea68154cafba47ba23f656afd9798d4
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2025-08-01 21:11:17 -0400

    Camera zoom thing on desk

--- Project/scripts/desk.gd
+++ Project/scripts/desk.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 func _on_day_switchboard_button_pressed() -> void:
-	Global.go_to_level("switchboard") # TODO
+	$Camera2D.zoom_to(1.0, $DayZoomTarget.global_position)
+	pass
 
 func _on_month_switchboard_button_pressed() -> void:
-	Global.go_to_level("switchboard") # TODO
+	$Camera2D.zoom_to(1.0, $MonthZoomTarget.global_position)
+	pass
 	
 func _on_year_switchboard_button_pressed() -> void:
-	Global.go_to_level("switchboard") # TODO
+	$Camera2D.zoom_to(1.0, $YearZoomTarget.global_position)
+	pass
 
 func _on_time_machine_button_pressed() -> void:
-	Global.go_to_level("time_input")
+	pass
+
+func _on_back_button_pressed() -> void:
+	$Camera2D.zoom_to(0.5, Vector2(0, 0))
+	pass
