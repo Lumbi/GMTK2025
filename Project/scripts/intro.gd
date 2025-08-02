825c39eced57b3dde5dd7c98d5f05a4c06e92210
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2025-08-01 21:30:47 -0400

    Go to desk after intro

--- Project/scripts/intro.gd
+++ Project/scripts/intro.gd
@@ -4,3 +4,8 @@ var test_dialogue = load("res://dialogues/test.dialogue")
 
 func _ready() -> void:
 	DialogueManager.show_dialogue_balloon(test_dialogue, "start")
+	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
+
+func on_dialogue_ended(_resource: DialogueResource):
+	Global.go_to_level("desk")
+	pass
