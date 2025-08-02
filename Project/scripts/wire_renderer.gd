b72af75d091b083328a8ca6be68a486a45382add
Hojaverde <jmprocco@gmail.com>
2025-08-02 16:53:43 -0400

    added plug to wire renderer

--- Project/scripts/wire_renderer.gd
+++ Project/scripts/wire_renderer.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 @onready var line2d: Line2D = $Line2D
+@onready var plug:Node2D = $plug
 
 @export var colors:Array[Color]
 
 
+func set_plug_position():
+	plug.global_position = line2d.points[line2d.points.size() - 1]
+	
+func _process(delta: float) -> void:
+	set_plug_position()
+	
 func _ready() -> void:
 	set_random_color()
 
