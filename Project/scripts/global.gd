9e3a2d4a631efade827d1fb7616fc370b7b0183d
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2025-08-02 12:36:18 -0400

    time input you win

--- Project/scripts/global.gd
+++ Project/scripts/global.gd
@@ -3,6 +3,9 @@ extends Node
 # This script is automatically loaded globally. 
 # You can use it as a singleton to handle logic from anywhere.
 
+func win_game():
+	go_to_level("win")
+
 func go_to_level(level: String):
 	get_tree().change_scene_to_file("res://levels/" + level + ".tscn")
 
--- Project/scripts/time_input.gd
+++ Project/scripts/time_input.gd
@@ -1,5 +1,20 @@
 extends Control
 
+# TODO SET THE RIGHT VALUES!!
+var answer_year = 2025
+var answer_month = 01
+var answer_day = 01
 
-func _on_button_pressed() -> void:
-	Global.go_to_level("desk")
+func _on_confirm_button_pressed() -> void:
+	var input_year = %YearInput.get_value()
+	var input_month = %MonthInput.get_value()
+	var input_day = %DayInput.get_value()
+
+	if input_year == answer_year and input_month == answer_month and input_day == answer_day:
+		print("YOU WIN!")
+		Global.win_game()
+	else:
+		print("NOPE")
+		# do a shake effect of something?
+
+	pass
