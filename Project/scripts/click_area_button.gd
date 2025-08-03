ecad931653b74d7e7622ee79e8e3075fbc10058a
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2025-08-03 09:24:30 -0400

    magnify glass

--- Project/scripts/click_area_button.gd
+++ Project/scripts/click_area_button.gd
@@ -1,9 +1,21 @@
 extends Button
 
+@export var magnify_cursor: Texture2D = preload("res://textures/cursor_magnify.png") 
+
 var starting_size: Vector2
+
+func on_mouse_hover_enter():
+	Input.set_custom_mouse_cursor(magnify_cursor)
+	pass
+	
+func on_mouse_hover_exit():
+	Input.set_custom_mouse_cursor(null)
+	pass
 
 func _ready() -> void:
 	starting_size = size
+	mouse_entered.connect(on_mouse_hover_enter)
+	mouse_exited.connect(on_mouse_hover_exit)
 
 func set_active(flag):
 	if flag:
