0071274186e7785d487c8ea4ca7af9b9d5359874
Dani Kogan <dani.kogandk@gmail.com>
2025-08-03 12:33:25 -0400

    Fixed Audio  and Dialog

--- Project/scripts/global_audio.gd
+++ Project/scripts/global_audio.gd
@@ -2,11 +2,61 @@ extends Node
 
 var sfx_dictionary: Dictionary = {}
 
+var sfx_dictionary2: Dictionary = {
+	"TvOn_crt" : [
+		 preload("res://audio/sfx/TvOn_crt.ogg")
+	],
+	"fail_sfx" : [
+		preload("res://audio/sfx/fail_sfx_1.ogg"),
+		preload("res://audio/sfx/fail_sfx_2.ogg"),
+		preload("res://audio/sfx/fail_sfx_3.ogg"),
+		preload("res://audio/sfx/fail_sfx_4.ogg"),
+		preload("res://audio/sfx/fail_sfx_5.ogg"),
+		preload("res://audio/sfx/fail_sfx_6.ogg"),
+		preload("res://audio/sfx/fail_sfx_7.ogg"),
+		preload("res://audio/sfx/fail_sfx_8.ogg"),
+		preload("res://audio/sfx/fail_sfx_9.ogg"),
+	],
+	"Static_Intense_Lp" : [
+		preload("res://audio/sfx/Static_Intense_Lp.ogg"),
+	],
+	"Static_Soft_Lp" : [
+		preload("res://audio/sfx/Static_Soft_Lp.ogg"),
+	],
+	"zoom_in" : [
+		preload("res://audio/sfx/zoom_in.ogg"),
+	],
+	"zoom_out" : [
+		preload("res://audio/sfx/zoom_out.ogg"),
+	],
+	"2025_GMTL_sfx_date_panel" : [
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_1.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_2.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_3.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_4.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_5.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_6.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_7.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_date_panel_8.ogg"),
+	],
+	"2025_GMTL_sfx_in" : [
+		preload("res://audio/sfx/2025_GMTL_sfx_in_1.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_in_2.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_in_3.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_in_4.ogg"),
+	],
+	"2025_GMTL_sfx_out" : [
+		preload("res://audio/sfx/2025_GMTL_sfx_out_1.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_out_2.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_out_3.ogg"),
+		preload("res://audio/sfx/2025_GMTL_sfx_out_4.ogg"),
+	],
+}
 var sfx_mix: Dictionary[String, float] = {
-	"2025_GMTL_sfx_in" : -6,
-	"2025_GMTL_sfx_out" : -6,
+	"2025_GMTL_sfx_in" : -12,
+	"2025_GMTL_sfx_out" : -12,
 	"2025_GMTL_sfx_date_panel" : -3,
-	"TvOn_crt" : -6,
+	"TvOn_crt" : -9,
 	"fail_sfx" : 0,
 	"zoom_in" : -18,
 	"zoom_out" :-18,
@@ -16,7 +66,8 @@ func get_audio_manager():
 	return AudioManager
 
 func _ready():
-	load_all_sfx_files()
+	pass
+	#load_all_sfx_files()
 
 func get_key_from_filename(file_name):
 	var key = file_name.get_basename()
@@ -58,9 +109,9 @@ func is_audio_file(file_name: String) -> bool:
 	return false
 
 func get_sfx(sfx_name: String) -> AudioStream:
-	if sfx_dictionary.has(sfx_name):
-		var random_num = randi() % sfx_dictionary[sfx_name].size()
-		return sfx_dictionary[sfx_name][random_num]
+	if sfx_dictionary2.has(sfx_name):
+		var random_num = randi() % sfx_dictionary2[sfx_name].size()
+		return sfx_dictionary2[sfx_name][random_num]
 	else:
 		print("Warning: SFX '", sfx_name, "' not found")
 		return null
